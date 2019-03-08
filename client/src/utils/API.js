e7b9b972a17f06b4860eb764bdc98805cb603da8
tcuddle <thcrump38@yahoo.com>
2019-03-08 15:48:44 -0600

    sec commit

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 
-// The getRecipes method retrieves recipes from the server
-// It accepts a "query" or term to search the recipe api for
 export default {
-  getRecipes: function(query) {
-    return axios.get("/api/recipes", { params: { q: query } });
+  // Gets all books
+  getBooks: function() {
+    return axios.get("/api/books");
+  },
+  // Gets the book with the given id
+  getBook: function(id) {
+    return axios.get("/api/books/" + id);
+  },
+  // Deletes the book with the given id
+  deleteBook: function(id) {
+    return axios.delete("/api/books/" + id);
+  },
+  // Saves a book to the database
+  saveBook: function(bookData) {
+    return axios.post("/api/books", bookData);
   }
 };
